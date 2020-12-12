c78aaa816b21a254fd003ccd4acb33b1b10dbfa3
kamishic <kamishi6@gmail.com>
2020-12-13 00:29:31 +0900

    todays commit

--- src/components/pdr/index.jsx
+++ src/components/pdr/index.jsx
@@ -2,12 +2,55 @@ import React,{useState,useEffect} from 'react'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { Link } from 'react-router-dom'
+import Box from '@material-ui/core/Box'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import dayjs from 'dayjs'
+
+import styled from 'styled-components';
+
+const StyledButton = styled(Button)`
+margin-top:16px;
+margin-bottom:16px;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+`;
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    width: "50%"
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+
 
 const getPdrs = async () => {
   console.log("start")
   const userId = 15
   const BASE_URL = `http://localhost:8080/api/v1/users/${userId}/pdrs` 
-
   const res = await fetch(BASE_URL)
   
   res.json().then(data => {
@@ -16,11 +59,14 @@ const getPdrs = async () => {
   })
 }
 
-
 const Index = () => {
   console.log("index")
   const [prepare,setPrepare] = useState([])
   
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+
   useEffect( async ()=> {
     const userId = 15
     const BASE_URL = `http://localhost:8080/api/v1/users/${userId}/pdrs` 
@@ -33,27 +79,37 @@ const Index = () => {
       let i = 0
 
       const listItems = data.map((x) =>
-        <ListItem>{x.thema}</ListItem>
+      <>
+        <StyledLink to= {`/`} >
+          <Card className={classes.root} variant="outlined">
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                {x.theme}
+              </Typography>
+              <Typography variant="h5" component="h2">
+                {x.prepare}
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                {dayjs(x.begin_date).format("YYYY-MM-DD")} 〜 {dayjs(x.end_date).format("YYYY-MM-DD")}
+              </Typography>
+            </CardContent>
+          </Card>
+        </StyledLink>
+        <br />
+      </>
       );
-      /*for (i in data) {
-        a = [...a,{prepare:data[i].prepare}]
-      }*/
+
       setPrepare(listItems)
     })
-
   },[])
 
   return (
     <div>
-      <List>
-        <Link to= "/pdr/new">
-          <ListItem>
-            新規作成
-          </ListItem>
-        </Link>
-        { prepare }
-
-      </List>
+      <StyledLink to= "/pdr/new">
+        <StyledButton variant="contained">新規作成</StyledButton>
+      </StyledLink>
+      
+      { prepare }
     </div>
   )
 }
--- src/components/pdr/new.jsx
+++ src/components/pdr/new.jsx
@@ -6,7 +6,8 @@ import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
+import dayjs from 'dayjs'
 
 const headers = new Headers()
 headers.set("Content-Type","application/json")
@@ -21,13 +22,16 @@ const getUser = async () => {
   console.log("test")
 }
 
-const createCycle = async (prepare,doing,beginDate,endDate) => {
+const createCycle = async (theme,prepare,doing,beginDate,endDate,term,doTimes) => {
   console.log("start")
   const body= {
+    theme: theme,
     prepare: prepare,
     doing: doing,
     beginDate: beginDate,
-    endDate: endDate
+    endDate: endDate,
+    term:term,
+    doTimes: doTimes
   }
   const BASE_URL = "http://localhost:8080/api/v1/users/15/pdrs" 
 
@@ -54,16 +58,31 @@ const New = () => {
   const [beginDate, setBeginDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [doTimes,setDoTimes] = useState(0)
+  const [term,setTerm] = useState(0)
 
   const calcDays = () => {
     if (beginDate != "" && endDate != "" && endDate >= beginDate) {
       var d1 = new Date(beginDate)
       var d2 = new Date(endDate)
-      return ((d2 - d1) / 86400000) + 1
+      const termCalc =  ((d2 - d1) / 86400000) + 1
+      alert(termCalc)
+      setTerm(termCalc)
     } else {
+      alert("else")
       return ""
     }
   }
+
+  const getDays = (e) => {
+    const [begin,end] = e
+    const beginDayjs = dayjs(begin)
+    const endDayjs = dayjs(end)
+    const termDayjs = endDayjs.diff(beginDayjs,'day') + 1
+    setBeginDate(beginDayjs.format("YYYY-MM-DD"))
+    setEndDate(endDayjs.format("YYYY-MM-DD"))
+    setTerm(termDayjs)
+  }
+
 
   return (
       <Grid container>
@@ -73,46 +92,36 @@ const New = () => {
             <TextField onChange = {(e) => setTheme(e.target.value)} id="theme" label="テーマを入力しましょう。" placeholder ="例．収入を増やす、ITスキル習得" fullWidth = "True" margin = "normal" variant="outlined"/>
           </ListItem>
           <ListItem>
-            <TextField onChange = {(e) => setPrepare(e.target.value)} id="prepare" label="テーマに基づく目標を設定しましょう。" placeholder ="例．アフィリエイトブログを作成する、Web技術の勉強をする。 " fullWidth = "True" margin = "normal" variant="outlined"/>
+            <TextField onChange = {(e) => setPrepare(e.target.value)} id="prepare" label="テーマに基づく目標を設定しましょう。" placeholder ="例．アフィリエイトブログを作成する、Web技術の勉強をする。" fullWidth = "True" margin = "normal" variant="outlined"/>
           </ListItem>
           <ListItem>
-            <TextField onChange = {(e) => setDoing(e.target.value)} id="doing" label="目標を実現するための具体的な行動を記入しましょう。" multiline rows={4} fullWidth = "True" margin = "normal" variant="outlined"/>
+            <TextField onChange = {(e) => setDoing(e.target.value)} id="doing" label="目標を実現するための具体的な行動を記入しましょう。"  placeholder ="例．週に○回記事を作成する　○○を読み込む" multiline rows={4} fullWidth = "True" margin = "normal" variant="outlined"/>
           </ListItem>
           <ListItem>
-            <TextField
-              id="beginDate"
-              label="開始日"
-              type="date"
-              defaultValue=""
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin = "dense"
-              variant="outlined"
-              onChange = {(e) => setBeginDate(e.target.value)}
-            />
+                <Calendar 
+                  calendarType = "US"
+                  maxDate = {new Date("2020","11","30")}
+                  minDate = {new Date("2020","10","0")}
+                  selectRange = {true}
+                  onChange = {(e)=>getDays(e,setBeginDate,setEndDate)}
+                />
+              <Grid item xs = {6}>
+                <ListItem>
+                  <TextField id="beginDate"　label="開始日"　margin = "dense"　variant="outlined"　value = {beginDate}/>
+                </ListItem>
+                <ListItem>
+                  <TextField id="endDate" label="終了日" placeholder="カレンダーで範囲を指定してください。" margin = "dense" variant="outlined" value = {endDate} />
+                </ListItem>
+                <ListItem>
+                  <TextField id="term" label="日数" margin = "dense" variant="outlined" value = {term}/>
+                </ListItem>
+                <ListItem>
+                 <TextField id="doTimes" onChange = {(e) => setDoTimes(e.target.value)} label="目標回数" margin = "dense" variant="outlined"/>
+                </ListItem>
+            </Grid>
           </ListItem>
           <ListItem>
-            <TextField
-              id="endDate"
-              label="終了日"
-              type="date"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin = "dense"
-              variant="outlined"
-              onChange = {(e) => setEndDate(e.target.value)}
-              />
-          </ListItem>
-          <ListItem>
-            <TextField label="日数" margin = "dense" variant="outlined" value = {calcDays()}/>
-          </ListItem>
-          <ListItem>
-            <TextField label="目標回数" margin = "dense" variant="outlined"/>
-          </ListItem>
-          <ListItem>
-            <Button color="primary" onClick = {() => createCycle(prepare,doing,beginDate,endDate)}>
+            <Button variant="contained" color="primary" onClick = {() => createCycle(theme,prepare,doing,beginDate,endDate,term,doTimes)}>
               作成
             </Button>
           </ListItem>
@@ -121,11 +130,6 @@ const New = () => {
       <Grid item xs={1}>
       </Grid>
       <Grid item xs={5}>
-        <Calendar 
-            calendarType = "US"
-            maxDate = {new Date("2020","11","30")}
-            minDate = {new Date("2020","10","0")}
-          />
       </Grid>
     </Grid>
   )
